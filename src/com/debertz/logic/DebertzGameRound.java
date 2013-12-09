26842b74e190a00514202816cd61a235502d25cc
Roman Bietin <diez.ukr@gmail.com>
2013-12-10 01:57:15 +0200

    game round API v0.1

--- src/com/debertz/logic/DebertzGameRound.java
+++ src/com/debertz/logic/DebertzGameRound.java
@@ -1,5 +1,6 @@
 package com.debertz.logic;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Random;
@@ -27,7 +28,7 @@ public class DebertzGameRound
 		debertzGamePlayers = players;
 		playersCount = players.length;
 		currentTrick = new PlayingCard[playersCount];
-		for(int i = 0; i < playersCount; i++)
+		for (int i = 0; i < playersCount; i++)
 			currentTrick[i] = null;
 		return !(playersCount < 2 || playersCount > 4);
 	}
@@ -40,19 +41,19 @@ public class DebertzGameRound
 	public boolean giveCards()
 	{
 		deck = new LinkedList<PlayingCard>();
-		for(PlayingCard.Rank rank : PlayingCard.Rank.values())
-			for(PlayingCard.Suit suit : PlayingCard.Suit.values())
+		for (PlayingCard.Rank rank : PlayingCard.Rank.values())
+			for (PlayingCard.Suit suit : PlayingCard.Suit.values())
 				deck.push(new PlayingCard(rank, suit));
 		/* shaking deck */
-		for(int i = 0; i < 1000; i++)
+		for (int i = 0; i < 1000; i++)
 		{
 			Random random = new Random(System.currentTimeMillis());
-			int card1 = random.nextInt() % deck.size();
-			int card2 = random.nextInt() % deck.size();
+			int card1 = Math.abs(random.nextInt()) % deck.size();
+			int card2 = Math.abs(random.nextInt()) % deck.size();
 			Collections.swap(deck, card1, card2);
 		}
-		for(int i = 0; i < 6; i++)
-			for(DebertzGamePlayer player : debertzGamePlayers)
+		for (int i = 0; i < 6; i++)
+			for (DebertzGamePlayer player : debertzGamePlayers)
 				player.addCard(deck.removeFirst());
 		sortPlayersHands();
 
@@ -64,8 +65,8 @@ public class DebertzGameRound
 	{
 		this.trumpSuit = trumpSuit;
 		int cardCount = 6 - playersCount;
-		for(int i = 0; i < cardCount; i++)
-			for(DebertzGamePlayer player : debertzGamePlayers)
+		for (int i = 0; i < cardCount; i++)
+			for (DebertzGamePlayer player : debertzGamePlayers)
 				player.addCard(deck.removeFirst());
 		sortPlayersHands();
 		return true;
@@ -73,14 +74,15 @@ public class DebertzGameRound
 
 	public boolean giveTalon(int turn)
 	{
-		if (turn < 0 || turn >= playersCount) return false;
+		if (turn < 0 || turn >= playersCount)
+			return false;
 		this.turn = turn;
 		return giveTalon(trumpCard.suit);
 	}
 
 	public LinkedList<PlayingCard> getPlayersHand(String name)
 	{
-		for(DebertzGamePlayer p : debertzGamePlayers)
+		for (DebertzGamePlayer p : debertzGamePlayers)
 			if (p.playerName.equals(name))
 				return p.hand;
 		return null;
@@ -88,14 +90,15 @@ public class DebertzGameRound
 
 	public PlayingCard getTopCard()
 	{
-		if (deck.isEmpty()) return null;
+		if (deck.isEmpty())
+			return null;
 		return deck.getFirst();
 	}
 
 	public boolean putCard(String name, PlayingCard card)
 	{
 		int playerID = -1;
-		for(int i = 0; i < playersCount; i++)
+		for (int i = 0; i < playersCount; i++)
 			if (debertzGamePlayers[i].playerName.equals(name))
 			{
 				playerID = i;
@@ -105,7 +108,7 @@ public class DebertzGameRound
 			return false;
 		DebertzGamePlayer player = debertzGamePlayers[playerID];
 		boolean validCard = false;
-		for(int i = 0; i < player.hand.size(); i++)
+		for (int i = 0; i < player.hand.size(); i++)
 		{
 			if (player.hand.get(i) == card)
 			{
@@ -116,21 +119,21 @@ public class DebertzGameRound
 		if (!validCard)
 			return false;
 
-		if(currentTrick[0] == null)
+		if (currentTrick[0] == null)
 		{
 			player.getCard(card);
 			currentTrick[0] = card;
 			return true;
 		}
 
-		if(currentTrick[0].suit != card.suit)
+		if (currentTrick[0].suit != card.suit)
 		{
 			if (player.hasSuit(currentTrick[0].suit))
 				return false;
 			if (card.suit != trumpSuit && player.hasSuit(trumpSuit))
 				return false;
 		}
-		for(int i = 1; i < playersCount; i++)
+		for (int i = 1; i < playersCount; i++)
 			if (currentTrick[i] == null)
 			{
 				player.getCard(card);
@@ -157,7 +160,8 @@ public class DebertzGameRound
 
 	private int getHighestCard(PlayingCard[] cards)
 	{
-		if (cards.length < 2 || cards.length > 4) return -1;
+		if (cards.length < 2 || cards.length > 4)
+			return -1;
 		int maxCardIndex = 0;
 		for (int i = 1; i < cards.length; i++)
 		{
@@ -178,7 +182,7 @@ public class DebertzGameRound
 
 	private void sortPlayersHands()
 	{
-		for(DebertzGamePlayer p: debertzGamePlayers)
+		for (DebertzGamePlayer p : debertzGamePlayers)
 			p.sortHand();
 	}
 
@@ -186,29 +190,82 @@ public class DebertzGameRound
 	{
 		if (card.suit == trumpSuit)
 		{
-			if(card.rank == PlayingCard.Rank.Jack) return 7;
-			if(card.rank == PlayingCard.Rank.Nine) return 6;
-			if(card.rank == PlayingCard.Rank.Ace) return 5;
-			if(card.rank == PlayingCard.Rank.Ten) return 4;
-			if(card.rank == PlayingCard.Rank.King) return 3;
-			if(card.rank == PlayingCard.Rank.Queen) return 2;
-			if(card.rank == PlayingCard.Rank.Eight) return 1;
-			if(card.rank == PlayingCard.Rank.Seven) return 1;
-		}
-		else
+			if (card.rank == PlayingCard.Rank.Jack)
+				return 7;
+			if (card.rank == PlayingCard.Rank.Nine)
+				return 6;
+			if (card.rank == PlayingCard.Rank.Ace)
+				return 5;
+			if (card.rank == PlayingCard.Rank.Ten)
+				return 4;
+			if (card.rank == PlayingCard.Rank.King)
+				return 3;
+			if (card.rank == PlayingCard.Rank.Queen)
+				return 2;
+			if (card.rank == PlayingCard.Rank.Eight)
+				return 1;
+			if (card.rank == PlayingCard.Rank.Seven)
+				return 1;
+		} else
 		{
-			if(card.rank == PlayingCard.Rank.Ace) return 7;
-			if(card.rank == PlayingCard.Rank.Ten) return 6;
-			if(card.rank == PlayingCard.Rank.King) return 5;
-			if(card.rank == PlayingCard.Rank.Queen) return 4;
-			if(card.rank == PlayingCard.Rank.Jack) return 3;
-			if(card.rank == PlayingCard.Rank.Nine) return 2;
-			if(card.rank == PlayingCard.Rank.Eight) return 1;
-			if(card.rank == PlayingCard.Rank.Seven) return 0;
+			if (card.rank == PlayingCard.Rank.Ace)
+				return 7;
+			if (card.rank == PlayingCard.Rank.Ten)
+				return 6;
+			if (card.rank == PlayingCard.Rank.King)
+				return 5;
+			if (card.rank == PlayingCard.Rank.Queen)
+				return 4;
+			if (card.rank == PlayingCard.Rank.Jack)
+				return 3;
+			if (card.rank == PlayingCard.Rank.Nine)
+				return 2;
+			if (card.rank == PlayingCard.Rank.Eight)
+				return 1;
+			if (card.rank == PlayingCard.Rank.Seven)
+				return 0;
 		}
 		return -1;
 	}
 
-
+	private LinkedList<DebertzCombination> getCombinations(DebertzGamePlayer player)
+	{
+		LinkedList<DebertzCombination> retval = new LinkedList<DebertzCombination>();
+		LinkedList<PlayingCard> combination = new LinkedList<PlayingCard>();
+		for (int i = 0; i < player.hand.size(); i++)
+		{
+			PlayingCard currentCard = player.hand.get(i);
+			if (combination.size() == 0)
+			{
+				combination.add(currentCard);
+				continue;
+			}
+			if (currentCard.suit == combination.getLast().suit
+					&& currentCard.rank.ordinal() - 1 == combination.getLast().rank.ordinal())
+			{
+				if (currentCard.suit == trumpSuit
+						&& currentCard.rank == PlayingCard.Rank.King
+						&& combination.getLast().rank == PlayingCard.Rank.Queen)
+				{
+					retval.add(
+							new DebertzCombination(
+									new LinkedList<PlayingCard>(
+											Arrays.asList(
+													new PlayingCard(PlayingCard.Rank.Queen, trumpSuit),
+													new PlayingCard(PlayingCard.Rank.King, trumpSuit)
+											))));
+				}
+				combination.add(currentCard);
+				if (i != player.hand.size() - 1)
+					continue;
+			}
+			if (combination.size() > 2)
+				retval.add(new DebertzCombination(combination));
+			combination = new LinkedList<PlayingCard>();
+		}
+		if (retval.size() == 0)
+			return null;
+		return retval;
+	}
 
 }
--- src/com/debertz/logic/PlayingCard.java
+++ src/com/debertz/logic/PlayingCard.java
@@ -39,6 +39,18 @@ public class PlayingCard
 		return rank + " " + suit;
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PlayingCard other = (PlayingCard) obj;
+		return suit == other.suit && rank == other.rank;
+	}
+
 	public static class PlayingCardComparator implements Comparator<PlayingCard>
 	{
 
