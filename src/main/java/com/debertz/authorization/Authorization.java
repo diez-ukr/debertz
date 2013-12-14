491b805b67ab549d96fbfcc92a9f1e26e69b6d02
Roman Bietin <diez.ukr@gmail.com>
2013-12-14 23:51:30 +0200

    Authorization stubs

---
+++ src/main/java/com/debertz/authorization/Authorization.java
@@ -0,0 +1,18 @@
+package com.debertz.authorization;/*
+ * Author: R.Bietin
+ * Date: 14.12.13
+ * Time: 23:48
+ */
+
+public class Authorization
+{
+	public static boolean Registrate(String username, String password)
+	{
+		return true;
+	}
+	public static boolean Authorizate(String username, String password)
+	{
+		return true;
+	}
+
+}
