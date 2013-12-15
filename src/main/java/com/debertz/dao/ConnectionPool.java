1e554b7e1166fd61f496328f1349c8bf00f1a891
Ievgen Luppol <eluppol@gmail.com>
2013-12-15 14:27:00 +0200

    connection pool added

--- src/main/java/com/debertz/dao/ConnectionPool.java
+++ src/main/java/com/debertz/dao/ConnectionPool.java
@@ -5,7 +5,7 @@ import java.util.concurrent.BlockingDeque;
 /**
  * Created by eluppol on 15.12.13.
  */
-public class ConnectionPull {
+public class ConnectionPool {
     BlockingDeque<Connection> connections;
     private static Connection connection = new Connection("ds039507.mongolab.com", 39507, "worker", "qwerty12345");
     public static Connection getConnection() {
