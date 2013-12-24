b322e5116dff06475e2a8d60fc97be7c26a1d7ec
Ievgen Luppol <eluppol@gmail.com>
2013-12-24 05:01:42 +0200

    added test table

--- src/main/java/com/debertz/logic/TablePool.java
+++ src/main/java/com/debertz/logic/TablePool.java
@@ -1,5 +1,7 @@
 package com.debertz.logic;
 
+import com.debertz.dao.Users;
+
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -7,8 +9,26 @@ import java.util.concurrent.atomic.AtomicLong;
  * Created by eluppol on 17.12.13.
  */
 public class TablePool {
+    private static Table deftable = makeTestTable();
+    private static Table makeTestTable() {
+        Table table = new Table(new TableParams(4, 1001), Users.getUser("eluppol"), -1);
+        table.join(Users.getUser("dima"));
+        table.join(Users.getUser("dimon"));
+        table.join(Users.getUser("diez"));
+        try {
+            table.getCurrentGame().nextRound();
+        } catch (PlayersCountException e) {
+            e.printStackTrace();
+        }
+        return table;
+    }
+    public static Table getTestTable() {
+        return deftable;
+    }
+
     private static CopyOnWriteArrayList<Table> tables = new CopyOnWriteArrayList<Table>();
     private static AtomicLong lastid = new AtomicLong(0);
+
     public static Table add(TableParams params, User creator) {
         Table table = new Table(params, creator, lastid.incrementAndGet());
         tables.add(table);
@@ -23,6 +43,9 @@ public class TablePool {
     }
 
     public static Table get(long id) {
+        if (id == -1) {
+            return deftable;
+        }
         for (Table table : tables) {
             if (table.getId() == id) {
                 return table;
